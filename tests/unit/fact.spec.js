68b4e32de464fd2e64dc1b8707cc74c9430741c2
brun00 <brunohenryt@hotmail.com>
2020-04-04 19:17:49 -0300

    sass variables for themes

--- tests/unit/fact.spec.js
+++ tests/unit/fact.spec.js
@@ -4,8 +4,8 @@ import Fact from '@/components/Fact'
 let wrapper = null
 const props = {
   id: 'myID',
-  message: 'new chuck fact',
-  link: 'https://api.chucknorris.io/jokes/hydtndocT6qyvBWxN7unTg',
+  value: 'new chuck fact',
+  url: 'https://api.chucknorris.io/jokes/hydtndocT6qyvBWxN7unTg',
   currentFact: 1,
   totalFacts: 99,
   active: false
@@ -20,11 +20,11 @@ afterEach(() => {
 })
 
 describe('Fact Component', () => {
-  it('renders prop message when passed', () => {
-    expect(wrapper.text()).toMatch(props.message)
+  it('renders prop value when passed', () => {
+    expect(wrapper.text()).toMatch(props.value)
   })
-  it('renders prop link when passed', () => {
-    expect(wrapper.find('.fact_info-link').attributes('href')).toBe(props.link)
+  it('renders prop url when passed', () => {
+    expect(wrapper.find('.fact_info-link').attributes('href')).toBe(props.url)
   })
   it('renders prop currentFact and totalFacts when passed', () => {
     const totals = `${props.currentFact}/${props.totalFacts}`
